427f34649284ad575d63056dcd655d05e56508d4
Chris Oliver <excid3@gmail.com>
2021-01-01 12:13:02 -0600

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+
+  has_one_attached :avatar
+  has_person_name
+
+  has_many :notifications, as: :recipient
+  has_many :services
+end
---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module DeviseHotwired
+  class Application < Rails::Application
+    config.active_job.queue_adapter = :sidekiq
+    config.application_name = Rails.application.class.module_parent_name
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
